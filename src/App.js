df5171c6727c16b498e78651ed21cb5a62b9893d
Nguyen Ngoc Hau (FIS DDCHCM) <haunn5@fpt.com.vn>
2022-10-18 17:17:29 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
+import PrivacyPolicy from "./privacy/PrivacyPolicy";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/stepbystep" element={<Home />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
